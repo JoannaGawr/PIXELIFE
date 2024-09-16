445521611b4b6c6ae508870cf6fcaca746d693e6
JG <gawryszewska.joanna@gmail.com>
2024-09-16 20:55:06 +0200

    Initial Commit

---
+++ PIXELife/PIXELifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PIXELifeApp.swift
+//  PIXELife
+//
+//  Created by Joanna Gawryszewska on 2024-09-16.
+//
+
+import SwiftUI
+
+@main
+struct PIXELifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
